9f7b88df40f029d7d4e9c485da07923094bd533a
Sachin Amingad <sachin.amingad@yahoo.com>
2021-07-29 17:35:08 +0530

    Made changes to driver function

---
+++ Driver/fatfs/port/sdc-hercules.h
@@ -0,0 +1,14 @@
+/*
+ * sdc-hercules.h
+ *
+ *  Created on: 29-Jul-2021
+ *      Author: reliance
+ */
+
+#ifndef DRIVER_FATFS_PORT_SDC_HERCULES_H_
+#define DRIVER_FATFS_PORT_SDC_HERCULES_H_
+
+
+void mmcSelectSpi(gioPORT_t *port, spiBASE_t *reg);
+
+#endif /* DRIVER_FATFS_PORT_SDC_HERCULES_H_ */
